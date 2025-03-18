ceeff54e38295ee945501f1a090e7f9aff9c4b38
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2025-03-17 19:49:47 -0600

    need libgnmawt.prx

--- source/main.c
+++ source/main.c
@@ -1051,8 +1051,8 @@ int main()
     // `/mnt/sandbox/pfsmnt/*-app0/` and `/mnt/sandbox/pfsmnt/*-patch0/`
     // i did this so when i pass in `/mnt/sandbox/pfsmnt` it will only dump `/mnt/sandbox/pfsmnt/PPSA01487-app0-patch0-union`
     // bc for ps5 games, `app0` and `app0-patch0-union` has the same files
-
-    dump_queue_add_dir(sock, "/mnt/sandbox/pfsmnt", 1);
+    dump_queue_add_dir(sock, "/system_ex/app/NPXS40140/cdc/lib", 1);
+    //dump_queue_add_dir(sock, "/mnt/sandbox/pfsmnt", 1);
     dump(sock, authmgr_handle, &offsets, "/data/dump");
 
 out:
