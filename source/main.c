999d1d70746ddeabf0125f0c6f8c7eae58f8f27c
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2025-08-24 11:08:52 -0600

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1105,7 +1105,8 @@ int main()
     // i did this so when i pass in `/mnt/sandbox/pfsmnt` it will only dump `/mnt/sandbox/pfsmnt/PPSA01487-app0-patch0-union`
     // bc for ps5 games, `app0` and `app0-patch0-union` has the same files
 
-    dump_queue_add_dir(sock, "/mnt/sandbox/pfsmnt", 1);
+    //dump_queue_add_dir(sock, "/mnt/sandbox/pfsmnt", 1);
+	dump_queue_add_dir(sock, "system_ex/common_ex/lib", 1);
     dump(sock, authmgr_handle, &offsets, "/data/dump");
 
 out:
