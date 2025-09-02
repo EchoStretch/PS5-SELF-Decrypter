ca170f8dc98489c18feb7bca8b8b632bb162c225
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2025-09-01 18:58:19 -0600

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1106,9 +1106,11 @@ int main()
     // bc for ps5 games, `app0` and `app0-patch0-union` has the same files
 
     //dump_queue_add_dir(sock, "/mnt/sandbox/pfsmnt", 1);
-	dump_queue_add_dir(sock, "system_ex/common_ex/lib", 1);
-    dump(sock, authmgr_handle, &offsets, "/data/dump");
-
+	//dump_queue_add_dir(sock, "system_ex/common_ex/lib", 1);
+    //dump(sock, authmgr_handle, &offsets, "/data/dump");
+    dump_queue_add_dir(sock, "/system", 1);
+    dump(sock, authmgr_handle, &offsets, "/mnt/usb0/data");
+	
 out:
 #ifdef LOG_TO_SOCKET
 	close(sock);
